a86ededc80645438117f59cff22a04fc4aafd32f
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-07-28 10:46:57 +1200

    fixed getPlayerCount method

--- src/view/UI.java
+++ src/view/UI.java
@@ -27,11 +27,49 @@ public class UI {
         System.out.println(initMessage);
     }
 
+    /**
+     * Gets the user to input the number of players for the Cluedo game.
+     *
+     * Called by {@link controller.GameController}
+     * and stored in the {@link model} package.
+     *
+     * @return Number of players.
+     */
     public int getPlayerCount() {
-        //TODO Add invalid input check
-        System.out.println("Enter the number of players (3-6)");
-        int playerCount = reader.nextInt();
-        return playerCount;
+        System.out.println("Enter the number of players (3-6):");
+        String input;
+
+        /* Continuously loops while valid input has not been found */
+        while (true) {
+            input = reader.nextLine();
+
+            // Checks if number input is integer and between 3 - 6
+            if (isValidInteger(input)) {
+                break;
+            }
+
+            System.out.println("Please enter a valid integer between 3 - 6");
+        }
+
+        // Parses the valid number and returns to GameController
+        return Integer.parseInt(input);
+    }
+
+    /**
+     * Helper method for {@link #getPlayerCount()}. Checks the validity of a users input to see
+     * if it is first a valid integer and then if it is between 3 - 6.
+     *
+     * @param input input from user via System.In
+     *
+     * @return The result of the validity check
+     */
+    private boolean isValidInteger(String input) {
+        try {
+            int valid = Integer.parseInt(input);
+            return valid >= 3 && valid <= 6;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
 }
