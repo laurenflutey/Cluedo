831425482da229a22d21fa7e214c58668d2af4f0
Reuben Puketapu <reuben.puketapu@gmail.com>
2015-08-03 15:40:41 +1200

    added set of cards

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,30 +1,43 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Marcel on 3/08/15.
  */
-public class Player extends Character{
+public class Player extends Character {
 
 	private Character character;
+	private Set<Card> cards;
 
-    /**
-     * Constructor for Character class
-     *
-     * @param name    the name of the Character
-     * @param ch
-     * @param xOrigin
-     * @param yOrigin
-     */
-    public Player(String name, char ch, int xOrigin, int yOrigin) {
-        super(name, ch, xOrigin, yOrigin);
-    }
+	/**
+	 * Constructor for Character class
+	 *
+	 * @param name
+	 *            the name of the Character
+	 * @param ch
+	 * @param xOrigin
+	 * @param yOrigin
+	 */
+	public Player(String name, char ch, int xOrigin, int yOrigin) {
+		super(name, ch, xOrigin, yOrigin);
+		this.cards = new HashSet<Card>();
+	}
 
-    public Character getCharacter() {
+	public Character getCharacter() {
 		return character;
 	}
 
 	public String getName() {
 		return character.getName();
+	}
+
+	/**
+	 * @return the cards
+	 */
+	public Set<Card> getCards() {
+		return cards;
 	}
 
 }
