1767abef02a0f9fad972c7649d0aa4ede10d5961
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-08-05 19:22:48 +1200

    documented entities

--- src/model/Entities.java
+++ src/model/Entities.java
@@ -22,7 +22,7 @@ public class Entities {
 	 *
 	 * @see Character
 	 */
-	private List<Character> characters;
+	private List<Character> characters = new ArrayList<>();
 
 	/**
 	 * Set of all weapons in the game, a random weapon will be selected on
@@ -30,7 +30,7 @@ public class Entities {
 	 *
 	 * @see Weapon
 	 */
-	private List<Weapon> weapons;
+	private List<Weapon> weapons = new ArrayList<>();
 
 	/**
 	 * Set of all rooms in the game. A random room will be selected as the room
@@ -38,46 +38,105 @@ public class Entities {
 	 *
 	 * @see Room
 	 */
-	private Map<String, Room> rooms;
+	private Map<String, Room> rooms = new HashMap<>();
 
-	private List<Card> cards;
+	/**
+	 * List of cards in the game. A card represents a Weapon, Room or Character
+	 */
+	private List<Card> cards = new ArrayList<>();
 
-	private List<Player> players;
+	/**
+	 * List of players in the game. A player is associated with a {@link Character}
+	 */
+	private List<Player> players = new ArrayList<>();
 
-	private List<Player> finalPlayers;
+	/**
+	 * A complete list of the players that never changes.
+	 */
+	private List<Player> finalPlayers = new ArrayList<>();
 
-	private Set<Card> winningCards;
+	/**
+	 * The solution to the game
+	 */
+	private Set<Card> winningCards = new HashSet<>();
 
+	/**
+	 * The cluedo game board
+	 */
 	private Board board;
 
+	/**
+	 * Constructor
+	 */
 	public Entities() {
 		init();
 	}
 
+	/**
+	 * Method to initialise all the data structures for the entities.
+	 */
 	private void init() {
+		// initialises character
+		initCharacters();
 
-		characters = new ArrayList<Character>();
-		finalPlayers = new ArrayList<Player>();
-		weapons = new ArrayList<Weapon>();
-		rooms = new HashMap<String, Room>();
-		cards = new ArrayList<Card>();
-		players = new ArrayList<>();
-		winningCards = new HashSet<Card>();
+		// initialises weapons
+		initWeapons();
 
-		characters.add(new Character("Mrs Peacock", 'p', 23, 6));
-		characters.add(new Character("Proffesor Plum", 'r', 23, 20));
-		characters.add(new Character("Miss Scarlet", 's', 7, 25));
-		characters.add(new Character("Colonel Mustard", 'm', 0, 18));
-		characters.add(new Character("Mrs White", 'w', 9, 0));
-		characters.add(new Character("Reverend Green", 'g', 14, 0));
+		// initialises rooms
+		initRooms();
 
-		weapons.add(new Weapon("Candlestick", '!'));
-		weapons.add(new Weapon("Dagger", '%'));
-		weapons.add(new Weapon("Lead Pipe", '?'));
-		weapons.add(new Weapon("Revolver", '&'));
-		weapons.add(new Weapon("Rope", '#'));
-		weapons.add(new Weapon("Spanner", '*'));
+		// initialises cards
+		initCards();
 
+		// shuffle the cards and weapons
+		Collections.shuffle(cards);
+		Collections.shuffle(weapons);
+
+		// finally create the game board
+		board = new Board(24, 26, rooms);
+	}
+
+	/**
+	 * Init method
+	 *
+	 * Assigns all of the cards to the collection of cards
+	 */
+	private void initCards() {
+		// Characters
+		cards.add(new Card("Mrs Peacock", "Character"));
+		cards.add(new Card("Mrs White", "Character"));
+		cards.add(new Card("Miss Scarlet", "Character"));
+		cards.add(new Card("Proffesor Plum", "Character"));
+		cards.add(new Card("Reverend Green", "Character"));
+		cards.add(new Card("Colonel Mustard", "Character"));
+
+		// Weapons
+		cards.add(new Card("Candlestick", "Weapon"));
+		cards.add(new Card("Dagger", "Weapon"));
+		cards.add(new Card("Lead Pipe", "Weapon"));
+		cards.add(new Card("Revolver", "Weapon"));
+		cards.add(new Card("Rope", "Weapon"));
+		cards.add(new Card("Spanner", "Weapon"));
+
+		// Rooms
+		cards.add(new Card("Kitchen", "Room"));
+		cards.add(new Card("Study", "Room"));
+		cards.add(new Card("Lounge", "Room"));
+		cards.add(new Card("Conservatory", "Room"));
+		cards.add(new Card("Ball Room", "Room"));
+		cards.add(new Card("Billiard Room", "Room"));
+		cards.add(new Card("Library", "Room"));
+		cards.add(new Card("Hall", "Room"));
+		cards.add(new Card("Dining Room", "Room"));
+	}
+
+	/**
+	 * Init method
+	 *
+	 * Creates the rooms, sets the connected rooms and then stores the rooms in the collection of rooms
+	 */
+	private void initRooms() {
+		// Create rooms
 		Room kitchen = new Room("Kitchen", 1, 'K');
 		Room study = new Room("Study", 2, 'S');
 		Room lounge = new Room("Lounge", 3, 'L');
@@ -89,11 +148,13 @@ public class Entities {
 		Room diningRoom = new Room("Dining Room", 9, 'D');
 		Room pool = new Room("Pool", 10, 'X');
 
+		// sets connected rooms
 		kitchen.setConnectingRoom(study);
 		study.setConnectingRoom(kitchen);
 		lounge.setConnectingRoom(conservatory);
 		conservatory.setConnectingRoom(lounge);
 
+		// store in data structure
 		rooms.put(conservatory.getName(), conservatory);
 		rooms.put(lounge.getName(), lounge);
 		rooms.put(kitchen.getName(), kitchen);
@@ -104,35 +165,34 @@ public class Entities {
 		rooms.put(library.getName(), library);
 		rooms.put(diningRoom.getName(), diningRoom);
 		rooms.put(pool.getName(), pool);
+	}
 
-		cards.add(new Card("Mrs Peacock", "Character"));
-		cards.add(new Card("Mrs White", "Character"));
-		cards.add(new Card("Miss Scarlet", "Character"));
-		cards.add(new Card("Proffesor Plum", "Character"));
-		cards.add(new Card("Reverend Green", "Character"));
-		cards.add(new Card("Colonel Mustard", "Character"));
+	/**
+	 * Init method
+	 *
+	 * Creates the weapons and adds them to the weapons collection
+	 */
+	private void initWeapons() {
+		weapons.add(new Weapon("Candlestick", '!'));
+		weapons.add(new Weapon("Dagger", '%'));
+		weapons.add(new Weapon("Lead Pipe", '?'));
+		weapons.add(new Weapon("Revolver", '&'));
+		weapons.add(new Weapon("Rope", '#'));
+		weapons.add(new Weapon("Spanner", '*'));
+	}
 
-		cards.add(new Card("Candlestick", "Weapon"));
-		cards.add(new Card("Dagger", "Weapon"));
-		cards.add(new Card("Lead Pipe", "Weapon"));
-		cards.add(new Card("Revolver", "Weapon"));
-		cards.add(new Card("Rope", "Weapon"));
-		cards.add(new Card("Spanner", "Weapon"));
-
-		cards.add(new Card("Kitchen", "Room"));
-		cards.add(new Card("Study", "Room"));
-		cards.add(new Card("Lounge", "Room"));
-		cards.add(new Card("Conservatory", "Room"));
-		cards.add(new Card("Ball Room", "Room"));
-		cards.add(new Card("Billiard Room", "Room"));
-		cards.add(new Card("Library", "Room"));
-		cards.add(new Card("Hall", "Room"));
-		cards.add(new Card("Dining Room", "Room"));
-
-		Collections.shuffle(cards);
-		Collections.shuffle(weapons);
-
-		board = new Board(24, 26, rooms);
+	/**
+	 * Init method
+	 *
+	 * Creates the characters and adds them to the characters collection
+	 */
+	private void initCharacters() {
+		characters.add(new Character("Mrs Peacock", 'p', 23, 6));
+		characters.add(new Character("Proffesor Plum", 'r', 23, 20));
+		characters.add(new Character("Miss Scarlet", 's', 7, 25));
+		characters.add(new Character("Colonel Mustard", 'm', 0, 18));
+		characters.add(new Character("Mrs White", 'w', 9, 0));
+		characters.add(new Character("Reverend Green", 'g', 14, 0));
 	}
 
 	/**
@@ -147,32 +207,58 @@ public class Entities {
 		return players.get(index);
 	}
 
+	/**
+	 * Gets characters.
+	 *
+	 * @return the characters
+     */
 	public List<Character> getCharacters() {
 		return characters;
 	}
 
+	/**
+	 * Gets weapons.
+	 *
+	 * @return the weapons
+     */
 	public List<Weapon> getWeapons() {
 		return weapons;
 	}
 
+	/**
+	 * Gets rooms.
+	 *
+	 * @return the rooms
+     */
 	public Map<String, Room> getRooms() {
 		return rooms;
 	}
 
+	/**
+	 * Gets cards.
+	 *
+	 * @return the cards
+     */
 	public List<Card> getCards() {
 		return cards;
 	}
 
+	/**
+	 * Gets players.
+	 *
+	 * @return the players
+     */
 	public List<Player> getPlayers() {
 		return players;
 	}
 
+	/**
+	 * Sets players.
+	 *
+	 * @param players the players
+     */
 	public void setPlayers(List<Player> players) {
 		this.players = players;
-	}
-
-	public void addPlayer(Player player) {
-		players.add(player);
 	}
 
 	/**
@@ -199,21 +285,11 @@ public class Entities {
 	}
 
 	/**
-	 * @param finalCharacters
-	 *            the finalCharacters to set
+	 * @param finalPlayers List of final Players in the game
 	 */
 	public void setFinalPlayers(List<Player> finalPlayers) {
 		this.finalPlayers = finalPlayers;
 	}
 
-	public Player getPlayerFromCharacter(Character character) {
-		for (Player player : players) {
-			if (player.getCharacter().getName().equals(character.getName())) {
-				return player;
-			}
-		}
-
-		return null;
-	}
 
 }
