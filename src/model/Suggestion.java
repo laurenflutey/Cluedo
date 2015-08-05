7df88c9a259cce72627a861e4bbc05d93d94c293
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-08-05 18:59:35 +1200

    added documentation to Suggestion

--- src/model/Suggestion.java
+++ src/model/Suggestion.java
@@ -1,11 +1,27 @@
 package model;
 
+/**
+ * Class to represent a Suggestion that a player can make. The class can also represent an accusation as well.
+ *
+ * A suggestion consists of a {@link Room} where the murder was committed, a {@link Weapon} with which the murder
+ * was committed, and a {@link Character} that committed the murder.
+ *
+ * @author Marcel
+ * @author Reuben
+ */
 public class Suggestion {
 
 	private Player player;
 	private Weapon weapon;
 	private Room room;
 
+	/**
+	 * Constructor
+	 *
+	 * @param player Player who committed the murder
+	 * @param weapon Weapon with which the murder was committed
+	 * @param room Room in which the murder was committed
+	 */
 	public Suggestion(Player player, Weapon weapon, Room room) {
 		this.player = player;
 		this.weapon = weapon;
