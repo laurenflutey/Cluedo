e530c0c127c7c46e2d88e8aa2bb47b0333bd099d
Marcel van Workum <Marcel.vanWorkum@gmail.com>
2015-08-23 12:54:52 +1200

    tidied up menubar

--- src/view/gui/game/components/GameMenu.java
+++ src/view/gui/game/components/GameMenu.java
@@ -14,32 +14,97 @@ import java.awt.event.ActionEvent;
  */
 public class GameMenu extends JMenuBar {
 
-    private final JMenu fileMenu;
-    private final JMenuItem selectBoardItem;
-    private final JMenuItem exitItem;
-    private final JMenu appearanceMenu;
-
+    /**
+     * Component containing the menu bar
+     */
     private final GameFrame PARENT;
 
-    public GameMenu(final GameFrame PARENT) {
+    // Menus
+    private JMenu fileMenu;
+    private JMenu appearanceMenu;
+    private JMenu themeMenu;
 
+    // Menu items
+    private JMenuItem selectBoardItem;
+    private JMenuItem exitItem;
+    private JMenuItem toggleZoomItem;
+    private JMenuItem theme1;
+    private JMenuItem theme2;
+    private JMenuItem theme3;
+    private JMenu soundMenu;
+    private JMenuItem toggleSoundItem;
+
+    /**
+     * Constructor
+     *
+     * Creates the menu components and delegates to init methods to create action listeners for each
+     *
+     * @param PARENT Parent container of the menu
+     */
+    public GameMenu(final GameFrame PARENT) {
         this.PARENT = PARENT;
 
+        // Remove ugly border
         setBorderPainted(false);
 
-        fileMenu = new JMenu("File");
-        add(fileMenu);
+        initFileMenu();
 
+        initAppearanceMenu();
+
+        initSoundMenu();
+    }
+
+    /**
+     * Creates the sound menu items and initialises their action listeners
+     */
+    private void initSoundMenu() {
+        soundMenu = new JMenu("Sound");
+        add(soundMenu);
+
+        toggleSoundItem = new JMenuItem("Toggle Sound"); //TODO implement sound
+        soundMenu.add(toggleSoundItem);
+    }
+
+    /**
+     * Menu that handles the multiple themes of the game, as well as the tile sizes.
+     *
+     * A theme change calls to the {@link GameCanvas} class to reload a different set of spritesheets
+     */
+    private void initAppearanceMenu() {
+        // Create menu
         appearanceMenu = new JMenu("Appearance");
         add(appearanceMenu);
 
-        JMenuItem toggleZoomItem = new JMenuItem(new AbstractAction("Toggle Zoom") {
+        // Toggle zoom item
+        toggleZoomItem = new JMenuItem(new AbstractAction("Toggle Zoom") {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Delegate to the canvas to toggle its size
                 PARENT.getCanvas().toggleTileSize();
             }
         });
         appearanceMenu.add(toggleZoomItem);
+
+        // Creates the theme menu, and the theme items
+        themeMenu = new JMenu("Theme");
+        appearanceMenu.add(themeMenu);
+
+        theme1 = new JMenuItem("Theme 1"); //TODO implement multiple themes
+        theme2 = new JMenuItem("Theme 2");
+        theme3 = new JMenuItem("Theme 3");
+
+        themeMenu.add(theme1);
+        themeMenu.add(theme2);
+        themeMenu.add(theme3);
+    }
+
+    /**
+     * Handles the closing and new game logic of the game. The new game delegates to eh {@link GuiGameController}
+     * to handle the closing of its components threads.
+     */
+    private void initFileMenu() {
+        fileMenu = new JMenu("File");
+        add(fileMenu);
 
         selectBoardItem = new JMenuItem(new AbstractAction("New Game") {
             @Override
@@ -57,22 +122,5 @@ public class GameMenu extends JMenuBar {
             }
         });
         fileMenu.add(exitItem);
-
-        JMenu themeMenu = new JMenu("Theme");
-        appearanceMenu.add(themeMenu);
-
-        JMenuItem theme1 = new JMenuItem("Theme 1");
-        JMenuItem theme2 = new JMenuItem("Theme 2");
-        JMenuItem theme3 = new JMenuItem("Theme 3");
-
-        themeMenu.add(theme1);
-        themeMenu.add(theme2);
-        themeMenu.add(theme3);
-
-        JMenu soundMenu = new JMenu("Sound");
-        add(soundMenu);
-
-        JMenuItem toggleSound = new JMenuItem("Toggle Sound");
-        soundMenu.add(toggleSound);
     }
 }
